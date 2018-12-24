edd0a8818dab34ea9d1d05326d9a361f60ac5a7d
Eric Sanderson <ersander@microsoft.com>
2018-12-24 15:54:02 -0800

    12_SendMessages

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+
+public class PlayerController : MonoBehaviour {
+
+    [Header("General")]
+    [Tooltip("In ms^-1")][SerializeField] float controlSpeed = 20f;
+    [Tooltip("In m")] [SerializeField] float xRange = 10.5f;
+    [Tooltip("In m")] [SerializeField] float yRange = 5f;
+    
+
+    [Header("Screen Position Parameters")]
+    [SerializeField] float positionPitchFactor = -3f;
+    [SerializeField] float positionYawFactor = 3f;
+
+    [Header("Controller Throw Parameters")]  
+    [SerializeField] float controlPitchFactor = -5f;
+    [SerializeField] float controlRollFactor = -13f;
+   
+    float xThrow, yThrow;
+    bool isControlEnabled = true;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (isControlEnabled)
+        {
+            ProcessTranslation();
+            ProcessRotation();
+        }
+                
+    }
+
+    // called by string message from Collision Handler
+    public void OnPlayerDeath()
+    {
+        if (Time.time > 10)
+        { 
+        isControlEnabled = false;
+        }
+    }
+
+
+    private void ProcessTranslation()
+    {
+        xThrow = CrossPlatformInputManager.GetAxis("Horizontal");
+        yThrow = CrossPlatformInputManager.GetAxis("Vertical");
+
+        float xOffset = xThrow * controlSpeed * Time.deltaTime;
+        float yOffset = yThrow * controlSpeed * Time.deltaTime;
+
+        float rawXPos = transform.localPosition.x + xOffset;
+        float rawYPos = transform.localPosition.y + yOffset;
+
+        float clampedXPos = Mathf.Clamp(rawXPos, -xRange, xRange);
+        float clampedYPos = Mathf.Clamp(rawYPos, -yRange, yRange);
+
+        transform.localPosition = new Vector3(clampedXPos, clampedYPos, transform.localPosition.z);
+    }
+
+
+    private void ProcessRotation()
+    {
+        float pitchDueToPosition = transform.localPosition.y * positionPitchFactor;
+        float pitchDueToControlThrow = yThrow * controlPitchFactor;
+
+        float yawDueToPosition = transform.localPosition.x * positionYawFactor;
+        
+        float pitch = pitchDueToPosition + pitchDueToControlThrow;
+        float yaw = yawDueToPosition;
+        float roll = xThrow * controlRollFactor;
+
+        transform.localRotation = Quaternion.Euler( pitch, yaw , roll ); 
+    }
+
+    
+
+
+}
+
+
+
