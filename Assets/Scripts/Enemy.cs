fa06d5fa7369497cb3116e3aa2468e298e928715
Eric Sanderson <ersander@microsoft.com>
2018-12-27 16:58:21 -0800

    19_Introducing Encapsulation_ScoreboardMsgs

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -5,19 +5,29 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour {
 
+    [SerializeField] int scorePerHit = 12;
+
     [SerializeField] GameObject deathFX;
     [SerializeField] Transform parent;
 
+    ScoreBoard scoreBoard;
+
 	// Use this for initialization
-	void Start () {
+	void Start ()
+    {
+        AddBoxCollider();
+        scoreBoard = FindObjectOfType<ScoreBoard>();
+    }
+
+    private void AddBoxCollider()
+    {
         Collider enemyBoxCollider = gameObject.AddComponent<BoxCollider>();
         enemyBoxCollider.isTrigger = false;
     }
 
-    
-
     void OnParticleCollision(GameObject other)
     {
+        scoreBoard.ScoreHit(scorePerHit); 
         GameObject fx = Instantiate(deathFX, transform.position, Quaternion.identity);
         fx.transform.parent = parent;
         Destroy(gameObject);
