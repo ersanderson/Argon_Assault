da38c73f2263194325806fe937ce9eb01ece230d
Eric Sanderson <ersander@microsoft.com>
2018-12-27 19:01:08 -0800

    21_EnemyHealthSystem

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -5,10 +5,12 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour {
 
-    [SerializeField] int scorePerHit = 12;
+
 
     [SerializeField] GameObject deathFX;
     [SerializeField] Transform parent;
+    [SerializeField] int scorePerHit = 12;
+    [SerializeField] int hits = 3;
 
     ScoreBoard scoreBoard;
 
@@ -27,11 +29,21 @@ public class Enemy : MonoBehaviour {
 
     void OnParticleCollision(GameObject other)
     {
-        scoreBoard.ScoreHit(scorePerHit); 
-        GameObject fx = Instantiate(deathFX, transform.position, Quaternion.identity);
-        fx.transform.parent = parent;
-        Destroy(gameObject);
+        scoreBoard.ScoreHit(scorePerHit);
+        hits = hits - 1;
+        // todo hit FX? 
+        if (hits <= 1)
+        {
+            KillEnemy();
+        }
+        
 
     }
 
+    private void KillEnemy()
+    {      
+        GameObject fx = Instantiate(deathFX, transform.position, Quaternion.identity);
+        fx.transform.parent = parent;
+        Destroy(gameObject);
+    }
 }
